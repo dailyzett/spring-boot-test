b846e5b542f4dd45e764e612138efebccafaeedb
sejun <dailyzett@gmail.com>
2023-09-03 23:58:53 +0900

    컨트롤러 레이어 단위 테스트
    - @WebMvcTest vs @SpringBootTest
    - @MockBean vs @Mock
    - Repository is not contains in spring container bean. Repository layer matches runtime.
    - Hamcrest library use
    - @SpringBootTest contains all Spring Container Bean
    - @WebMvcTest contains test for only uses Controller Layer
    - @WebMvcTest can load specific controller class

---
+++ src/main/java/net/jun/springboottest/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package net.jun.springboottest.controller;
+
+import lombok.RequiredArgsConstructor;
+import net.jun.springboottest.model.Employee;
+import net.jun.springboottest.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+@RequiredArgsConstructor
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+}
---
+++ src/test/java/net/jun/springboottest/controller/EmployeeControllerTest.java
@@ -0,0 +1,57 @@
+package net.jun.springboottest.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.jun.springboottest.model.Employee;
+import net.jun.springboottest.service.EmployeeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class EmployeeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private EmployeeService employeeService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void givenEmployeeObject_whenCreateEmployee_thenReturnSavedEmployee() throws Exception {
+        //given
+        Employee employee = Employee.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .email("JohnDoe@gmail.com")
+                .build();
+        given(employeeService.saveEmployee(any(Employee.class)))
+                .willAnswer((invocation) -> invocation.getArguments()[0]);
+
+        //when
+        ResultActions response = mockMvc.perform(post("/api/employees")
+                .contentType(APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(employee)));
+
+        //then
+        response.andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName", is(employee.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(employee.getLastName())))
+                .andExpect(jsonPath("$.email", is(employee.getEmail())));
+    }
+}
